b93a295c283698a67ad7bffdca28451e09403a66
AmeliaDhea <120483575+AmeliaDhea@users.noreply.github.com>
2023-11-29 01:02:12 +0700

    Add files via upload

---
+++ UAP DAP _Amelia Dhea Puspita/script.js
@@ -0,0 +1,48 @@
+document.getElementById('homeIcon').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
+document.getElementById('iconAboutMe').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
+document.getElementById('iconPengalamanKerja').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
+document.getElementById('IconPengalamanPendidikan').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
+document.getElementById('iconContact').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
+document.getElementById('iconInstagram').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
+document.getElementById('iconWA').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
+document.getElementById('iconLinkedIn').addEventListener('click', function() {
+    var link = this.getAttribute('data-link');
+    if (link) {
+        window.location.href = link; // Buka URL baru
+    }
+});
